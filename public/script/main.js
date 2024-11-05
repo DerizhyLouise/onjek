2179316cb4658a8b9260bbe8e85cf4ad639b2e7c
DerizhyLouise <louise.derizhy@gmail.com>
2024-11-05 23:06:13 +0700

    add FE

---
+++ public/script/main.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				black: "#013220",
+				darkgreen: "#014421",
+				green: "#355E3B",
+				sage: "#BCB88A",
+				cuswhite: "#FBFBFB"
+			},
+		},
+	},
+};
+
+// const session = sessionStorage.getItem("user");
+// if (!session && !window.location.pathname.startsWith("/app/login") && !window.location.pathname.startsWith("/app/register")) {
+//     window.location.href = "/app/login";
+// }
